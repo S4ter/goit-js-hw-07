bfd539c65e63a9c8af65fe762c2b8e7d2476f3af
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-04-20 22:46:14 +0200

    ad 1 done, added gallery and close on esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function addNewGalleryItem(newItem) {
+  newItem.forEach((el) => {
+    const newGalleryItem = `<li class='gallery__item'><img src='${el.preview}' alt='${el.description}' class='gallery__image' data-original='${el.original}' /></li>`;
+    gallery.insertAdjacentHTML("beforeend", newGalleryItem);
+  });
+}
+addNewGalleryItem(galleryItems);
+function showOriginal(event) {
+  const galleryImage = event.target;
+  const originalSrc = galleryImage.dataset.original;
+  const instance = basicLightbox.create(`<img src="${originalSrc}"/>`);
+  instance.show();
+
+  function closeOnEscape(event) {
+    event.preventDefault();
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+  document.addEventListener("keydown", closeOnEscape);
+}
+
+const galleryImages = document.querySelectorAll(".gallery__image");
+galleryImages.forEach((image) => {
+  image.addEventListener("click", showOriginal);
+});
