4f1c70fdd93d78204e0daf1bafeafaf732935f16
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-04-24 14:38:42 +0200

    ad 2 finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function addNewGalleryItem(newItem) {
+  newItem.forEach((el) => {
+    const newGalleryItem = `
+    <li><a class="gallery__item" href='${el.original}' alt='${el.description}'>
+      <img class="gallery__image" src='${el.preview}' alt='${el.description}' />
+    </a>
+    </li>`;
+    gallery.insertAdjacentHTML("beforeend", newGalleryItem);
+  });
+}
+addNewGalleryItem(galleryItems);
+
+let newGallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
+newGallery.on("show.simplelightbox", function () {});
